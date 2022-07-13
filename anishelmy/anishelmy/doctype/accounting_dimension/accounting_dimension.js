96c6593466a1367920dbcafdc24fd8c6213f52c1
mghoneem10 <mghoneem10@gmail.com>
2022-07-13 21:03:51 +0000

    Export to frappe cloud

---
+++ anishelmy/anishelmy/doctype/accounting_dimension/accounting_dimension.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, erpcloud.systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Accounting Dimension', {
+	// refresh: function(frm) {
+
+	// }
+});
